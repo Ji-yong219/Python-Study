29a661b44a7cbb1ea7e288886e21323aa26a9af6
Ji-yong219 <76960714+Ji-yong219@users.noreply.github.com>
2021-12-27 01:04:48 +0900

    Update MethodOverloading.py

--- MethodOverloading.py
+++ MethodOverloading.py
@@ -1,48 +1,48 @@
 class Cls():
-	def do(self):
+	def foo(self):
 		print("Nothing")
 		
-	def do(self, num):
+	def foo(self, num):
 		print(f"Number : {num}")
 		
-	def do(self, num, String):
+	def foo(self, num, String):
 		print(f"Number : {num}\tString : {String}")
 		
-	def do(self, num, String, Lst):
+	def foo(self, num, String, Lst):
 		print(f"Number : {num}\tString : {String}\tList : {Lst}")
 		
 from functools import singledispatch
 import numbers
 class ClsWithSingleDispatch():
 	@singledispatch
-	def do(self):
+	def foo(self):
 		print("Nothing")
 		
-	@do.register(int)
+	@foo.register(int)
 	def _(self, num:int):
 		print(f"Number : {num}")
 		
-	@do.register(int)
-	@do.register(str)
+	@foo.register(int)
+	@foo.register(str)
 	def _(self, num:int, String:str):
 		print(f"Number : {num}\tString : {String}")
 		
-	@do.register(int)
-	@do.register(str)
-	@do.register(list)
+	@foo.register(int)
+	@foo.register(str)
+	@foo.register(list)
 	def _(self, num:int, String:str, Lst:list):
 		print(f"Number : {num}\tString : {String}\tList : {Lst}")
 		
 
 if __name__ == "__main__":
 	csd = ClsWithSingleDispatch()
-	csd.do()
-	csd.do(2)
-	csd.do(2, "Two")
-	csd.do(2, "Two", [2, "Two"])
+	csd.foo()
+	csd.foo(2)
+	csd.foo(2, "Two")
+	csd.foo(2, "Two", [2, "Two"])
 	
 	c = Cls()
-	c.do()
-	c.do(2)
-	c.do(2, "Two")
-	c.do(2, "Two", [2, "Two"])
+	c.foo()
+	c.foo(2)
+	c.foo(2, "Two")
+	c.foo(2, "Two", [2, "Two"])
