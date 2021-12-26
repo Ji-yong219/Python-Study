839b7703dea2a4d0658c02122360c7caae6c3c89
Ji-yong219 <76960714+Ji-yong219@users.noreply.github.com>
2021-12-27 01:19:43 +0900

    Update MethodOverloading.py

--- MethodOverloading.py
+++ MethodOverloading.py
@@ -1,48 +1,35 @@
 class Cls():
-	def foo(self):
-		print("Nothing")
-		
-	def foo(self, num):
+	def foo(self, num:int):
 		print(f"Number : {num}")
 		
-	def foo(self, num, String):
-		print(f"Number : {num}\tString : {String}")
+	def foo(self, String:str):
+		print(f"String : {String}")
 		
-	def foo(self, num, String, Lst):
-		print(f"Number : {num}\tString : {String}\tList : {Lst}")
+	def foo(self, Lst:list):
+		print(f"List : {Lst}")
 		
 from functools import singledispatch
-import numbers
 class ClsWithSingleDispatch():
 	@singledispatch
-	def foo(self):
-		print("Nothing")
-		
-	@foo.register(int)
-	def _(self, num:int):
+	def foo(self, num:int):
 		print(f"Number : {num}")
 		
-	@foo.register(int)
 	@foo.register(str)
-	def _(self, num:int, String:str):
-		print(f"Number : {num}\tString : {String}")
+	def _(self, String:str):
+		print(f"String : {String}")
 		
-	@foo.register(int)
-	@foo.register(str)
 	@foo.register(list)
-	def _(self, num:int, String:str, Lst:list):
-		print(f"Number : {num}\tString : {String}\tList : {Lst}")
+	def _(self, Lst:list):
+		print(f"List : {Lst}")
 		
 
 if __name__ == "__main__":
 	csd = ClsWithSingleDispatch()
-	csd.foo()
 	csd.foo(2)
-	csd.foo(2, "Two")
-	csd.foo(2, "Two", [2, "Two"])
+	csd.foo("Two")
+	csd.foo([2, "Two"])
 	
 	c = Cls()
-	c.foo()
 	c.foo(2)
-	c.foo(2, "Two")
-	c.foo(2, "Two", [2, "Two"])
+	c.foo("Two")
+	c.foo([2, "Two"])
